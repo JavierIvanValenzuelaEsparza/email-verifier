c135b130215b8e5be2dee2adb73d7dc61ccc3ee8
JavierValenzuelaEsparza <javier.valenzuela@g-global.com>
2025-12-02 10:12:21 -0800

    feat(testimonials): add testimonials users in project ms email-verifier by my portfolio

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   }));
   
   app.enableCors({
-    origin: '*',
+    origin: 'true',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
